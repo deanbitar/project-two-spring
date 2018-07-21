75efd268e97eb39f015f4cdc49aac6c1f17104c0
Ezzdean Bietar <EzzdeanB@gmail.com>
2018-07-21 15:52:19 -0400

    Added getUserByEmail

--- ChordSpring/src/main/java/com/chord/dao/UserDao.java
+++ ChordSpring/src/main/java/com/chord/dao/UserDao.java
@@ -17,38 +17,41 @@ public class UserDao {
 	static {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-		}catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Autowired
 	private SessionFactory sessionFactory;
-	
+
 	public UserDao() {
-		
+
 	}
-	
+
 	public void insert(User user) {
 		sessionFactory.getCurrentSession().save(user);
 	}
-	
+
 	public void update(User user) {
 		sessionFactory.getCurrentSession().update(user);
 	}
-	
+
 	public User selectById(int id) {
 		System.out.println("Retrieveing user with id: " + id);
 		User user = sessionFactory.getCurrentSession().get(User.class, id);
 		System.out.println(user);
 		return user;
 	}
-	
+
 	public User selectByEmail(String email) {
-		
-		return null;
+
+		User user = sessionFactory.getCurrentSession().createQuery("from Users where email = " + email, User.class)
+				.uniqueResult();
+
+		return user;
 	}
-	
+
 	public List<User> selectAll() {
 		return sessionFactory.getCurrentSession().createQuery("from Users", User.class).list();
 	}
