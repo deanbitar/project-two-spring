bc81569df78b16bbefb0bc202d2cce35fe7a20a6
Ezzdean Bietar <EzzdeanB@gmail.com>
2018-07-21 16:12:00 -0400

    Added CORS origin

--- ChordSpring/src/main/java/com/chord/controller/PostController.java
+++ ChordSpring/src/main/java/com/chord/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,6 +14,7 @@ import com.chord.model.Post;
 import com.chord.model.User;
 
 @Controller
+@CrossOrigin(origins="http://localhost:4200")
 public class PostController {
 	
 	@Autowired
--- ChordSpring/src/main/java/com/chord/controller/UserController.java
+++ ChordSpring/src/main/java/com/chord/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +13,7 @@ import com.chord.dao.UserDao;
 import com.chord.model.User;
 
 @Controller
+@CrossOrigin(origins="http://localhost:4200")
 public class UserController {
 
 	@Autowired
