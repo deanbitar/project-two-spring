e95bdd8034a573a31c52179382d053b828316e03
Ezzdean Bietar <EzzdeanB@gmail.com>
2018-07-21 20:08:07 -0400

    testing new login.chord

--- ChordSpring/src/main/java/com/chord/controller/UserController.java
+++ ChordSpring/src/main/java/com/chord/controller/UserController.java
@@ -3,6 +3,10 @@ package com.chord.controller;
 import java.util.List;
 import java.util.Set;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,6 +20,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.chord.dao.UserDao;
 import com.chord.model.User;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 
 @Controller
 @CrossOrigin(origins="http://localhost:4200")
@@ -77,7 +83,14 @@ public class UserController {
 	}
 	
 	@PostMapping(value="/login.chord")
-	public @ResponseBody User login(String email, String password) {
+	public @ResponseBody User login(HttpServletRequest request, HttpServletResponse response) {
+		
+		System.out.println(request.getParameterNames().nextElement());
+		
+		JSONObject object = new JSONObject(request.getParameterNames().nextElement());
+		
+		String email = object.getString("email");
+		String password = object.getString("password");
 		
 		System.out.println("New login request with " + email + " " + password);
 		
